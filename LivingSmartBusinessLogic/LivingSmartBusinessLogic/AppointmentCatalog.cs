3b0f6d3ebf3250d6dfa406196fd75d5665361ed6
Mathias Poulsen <mathias-poulsen@hotmail.com>
2015-05-11 14:38:53 +0200

    Catalogs

---
+++ LivingSmartBusinessLogic/LivingSmartBusinessLogic/AppointmentCatalog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LivingSmartBusinessLogic
+{
+    public class AppointmentCatalog
+    {
+        private System.Collections.Generic.List<LivingSmartBusinessLogic.Appointment> appointments;
+    }
+}
