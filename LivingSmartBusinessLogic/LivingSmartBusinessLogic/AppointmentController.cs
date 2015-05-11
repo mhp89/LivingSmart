d83223a60400fa7127f3111766bd3301895a4b52
Mathias Poulsen <mathias-poulsen@hotmail.com>
2015-05-11 15:15:22 +0200

    Klassediagram tættere på at være færdigt
    Diverse controllere tilføjer

---
+++ LivingSmartBusinessLogic/LivingSmartBusinessLogic/AppointmentController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LivingSmartBusinessLogic
+{
+    public class AppointmentController
+    {
+    }
+}
