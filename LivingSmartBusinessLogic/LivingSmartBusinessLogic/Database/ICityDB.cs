39f0aef56febb4540d2b449bc4e5b581a4a77e89
René Sørensen <rensoe@gmail.com>
2015-05-29 10:56:08 +0200

    Making me own branch.

---
+++ LivingSmartBusinessLogic/LivingSmartBusinessLogic/Database/ICityDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivingSmartBusinessLogic.DB
+{
+    internal interface ICityDB
+    {
+        List<City> ReadCities();
+        void UpdateCity(City city);
+        int CreateCity(City city);
+    }
+}
