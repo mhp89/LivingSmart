a462452be2b186cfcaefca0bf15ad4c167f5bbf4
René Sørensen <rensoe@gmail.com>
2015-05-29 11:23:17 +0200

    Added CityDB

---
+++ LivingSmartBusinessLogic/LivingSmartBusinessLogic/Database/CityDBMSSQL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LivingSmartBusinessLogic.DBLayer;
+
+namespace LivingSmartBusinessLogic.DB
+{
+    internal class CityDBMSSQL : ICityDB
+    {
+        public List<City> ReadCities()
+        {
+            List<City> cityList = new List<City>();
+            SqlConnection connection = DBConnectionMSSQL.Instance.GetDBConnection();
+            SqlCommand cmd = new SqlCommand
+            {
+                Connection = connection,
+                CommandText = "SELECT * FROM City;",
+            };
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int zipCode = (int)reader["ZipCode"];
+                    string district = (string)reader["District"];
+
+                    City city = new City(zipCode, district);
+                    cityList.Add(city);
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return cityList;
+        }
+    }
+}
