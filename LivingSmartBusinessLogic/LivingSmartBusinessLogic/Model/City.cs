69e7e1e1e059eb5a0585153062c83104a9f1642c
cmisop <Mathias.s.petersen@live.dk>
2015-05-29 08:58:43 +0200

    Lidt flere filer

--- LivingSmartBusinessLogic/LivingSmartBusinessLogic/Model/City.cs
+++ LivingSmartBusinessLogic/LivingSmartBusinessLogic/Model/City.cs
@@ -17,7 +17,7 @@ namespace LivingSmartBusinessLogic
         #region Properties
 
         public int ZipCode { get { return _zipcode; } internal set { _zipcode = value; } }
-        public string Seller { get { return _district; } internal set { _district = value; } }
+		public string District { get { return _district; } internal set { _district = value; } }
 
         #endregion
 
