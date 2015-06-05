ef9baf9907af3717a37488ba7eae9172c8fd1c78
Maja Olesen <maja_olesen@hotmail.com>
2015-06-05 12:13:00 +0200

    Udseende af dials på forsiden rettet
    overskrift og labels tilføjet og placering ændret

--- LivingSmartBusinessLogic/LivingSmartForms/Pages/Overview.Designer.cs
+++ LivingSmartBusinessLogic/LivingSmartForms/Pages/Overview.Designer.cs
@@ -30,89 +30,189 @@ namespace LivingSmartForms.Pages
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.clsActiveCases = new SmartControls.ControlList();
-			this.header1 = new SmartControls.Header();
-			this.meter2 = new GraphicalMeterLibrary.Meter();
-			this.meter1 = new GraphicalMeterLibrary.Meter();
-			this.SuspendLayout();
-			// 
-			// clsActiveCases
-			// 
-			this.clsActiveCases.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.clsActiveCases = new SmartControls.ControlList();
+            this.mtrCount = new GraphicalMeterLibrary.Meter();
+            this.lblCountTitle = new System.Windows.Forms.Label();
+            this.hdrOngoingCases = new SmartControls.Header();
+            this.hdrDials = new SmartControls.Header();
+            this.pnlNoOfProperties = new System.Windows.Forms.Panel();
+            this.lblCountValue = new System.Windows.Forms.Label();
+            this.pnlPrice = new System.Windows.Forms.Panel();
+            this.lblPriceValue = new System.Windows.Forms.Label();
+            this.mtrPrice = new GraphicalMeterLibrary.Meter();
+            this.lblPriceTitle = new System.Windows.Forms.Label();
+            this.pnlNoOfProperties.SuspendLayout();
+            this.pnlPrice.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clsActiveCases
+            // 
+            this.clsActiveCases.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.clsActiveCases.AutoScroll = true;
-			this.clsActiveCases.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-			this.clsActiveCases.Location = new System.Drawing.Point(20, 301);
-			this.clsActiveCases.Margin = new System.Windows.Forms.Padding(20, 10, 20, 20);
-			this.clsActiveCases.Name = "clsActiveCases";
-			this.clsActiveCases.Size = new System.Drawing.Size(1095, 239);
-			this.clsActiveCases.TabIndex = 6;
-			this.clsActiveCases.WrapContents = false;
-			// 
-			// header1
-			// 
-			this.header1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.clsActiveCases.AutoScroll = true;
+            this.clsActiveCases.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.clsActiveCases.Location = new System.Drawing.Point(20, 243);
+            this.clsActiveCases.Margin = new System.Windows.Forms.Padding(20, 10, 20, 20);
+            this.clsActiveCases.Name = "clsActiveCases";
+            this.clsActiveCases.Size = new System.Drawing.Size(1095, 297);
+            this.clsActiveCases.Spacing = 5;
+            this.clsActiveCases.TabIndex = 6;
+            this.clsActiveCases.WrapContents = false;
+            // 
+            // mtrCount
+            // 
+            this.mtrCount.CircleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.mtrCount.CirclePoints = 3;
+            this.mtrCount.CircleSize = 175;
+            this.mtrCount.Location = new System.Drawing.Point(0, 30);
+            this.mtrCount.Margin = new System.Windows.Forms.Padding(20);
+            this.mtrCount.MaxValue = 60;
+            this.mtrCount.MinValue = 0;
+            this.mtrCount.Name = "mtrCount";
+            this.mtrCount.PointerColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.mtrCount.Size = new System.Drawing.Size(200, 90);
+            this.mtrCount.TabIndex = 0;
+            this.mtrCount.Text = "meter1";
+            this.mtrCount.Value = 54;
+            // 
+            // lblCountTitle
+            // 
+            this.lblCountTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblCountTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.lblCountTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblCountTitle.Name = "lblCountTitle";
+            this.lblCountTitle.Size = new System.Drawing.Size(200, 20);
+            this.lblCountTitle.TabIndex = 7;
+            this.lblCountTitle.Text = "Antal ejendomme";
+            this.lblCountTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // hdrOngoingCases
+            // 
+            this.hdrOngoingCases.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.header1.Color = SmartControls.SmartColor.ColorStyle.Light;
-			this.header1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-			this.header1.Location = new System.Drawing.Point(10, 266);
-			this.header1.Name = "header1";
-			this.header1.Size = new System.Drawing.Size(1115, 25);
-			this.header1.TabIndex = 5;
-			this.header1.Text = "Igangværende sager";
-			// 
-			// meter2
-			// 
-			this.meter2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.meter2.CircleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-			this.meter2.CirclePoints = 3;
-			this.meter2.CircleSize = 175;
-			this.meter2.Location = new System.Drawing.Point(915, 20);
-			this.meter2.Margin = new System.Windows.Forms.Padding(20);
-			this.meter2.MaxValue = 60;
-			this.meter2.MinValue = 0;
-			this.meter2.Name = "meter2";
-			this.meter2.PointerColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
-			this.meter2.Size = new System.Drawing.Size(200, 100);
-			this.meter2.TabIndex = 1;
-			this.meter2.Text = "meter2";
-			this.meter2.Value = 29;
-			// 
-			// meter1
-			// 
-			this.meter1.CircleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-			this.meter1.CirclePoints = 3;
-			this.meter1.CircleSize = 175;
-			this.meter1.Location = new System.Drawing.Point(20, 20);
-			this.meter1.Margin = new System.Windows.Forms.Padding(20);
-			this.meter1.MaxValue = 60;
-			this.meter1.MinValue = 0;
-			this.meter1.Name = "meter1";
-			this.meter1.PointerColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
-			this.meter1.Size = new System.Drawing.Size(200, 100);
-			this.meter1.TabIndex = 0;
-			this.meter1.Text = "meter1";
-			this.meter1.Value = 29;
-			// 
-			// Overview
-			// 
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.Controls.Add(this.clsActiveCases);
-			this.Controls.Add(this.header1);
-			this.Controls.Add(this.meter2);
-			this.Controls.Add(this.meter1);
-			this.Name = "Overview";
-			this.Size = new System.Drawing.Size(1135, 560);
-			this.ResumeLayout(false);
+            this.hdrOngoingCases.Color = SmartControls.SmartColor.ColorStyle.Light;
+            this.hdrOngoingCases.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.hdrOngoingCases.Location = new System.Drawing.Point(10, 208);
+            this.hdrOngoingCases.Margin = new System.Windows.Forms.Padding(10, 20, 10, 0);
+            this.hdrOngoingCases.Name = "hdrOngoingCases";
+            this.hdrOngoingCases.Size = new System.Drawing.Size(1115, 25);
+            this.hdrOngoingCases.TabIndex = 9;
+            this.hdrOngoingCases.Text = "Igangværende sager";
+            // 
+            // hdrDials
+            // 
+            this.hdrDials.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hdrDials.Color = SmartControls.SmartColor.ColorStyle.Light;
+            this.hdrDials.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.hdrDials.Location = new System.Drawing.Point(10, 0);
+            this.hdrDials.Name = "hdrDials";
+            this.hdrDials.Size = new System.Drawing.Size(1115, 25);
+            this.hdrDials.TabIndex = 10;
+            this.hdrDials.Text = "Ejendomme solgt i indeværende år sammenlignet med sidste år";
+            // 
+            // pnlNoOfProperties
+            // 
+            this.pnlNoOfProperties.Controls.Add(this.lblCountValue);
+            this.pnlNoOfProperties.Controls.Add(this.mtrCount);
+            this.pnlNoOfProperties.Controls.Add(this.lblCountTitle);
+            this.pnlNoOfProperties.Location = new System.Drawing.Point(20, 35);
+            this.pnlNoOfProperties.Name = "pnlNoOfProperties";
+            this.pnlNoOfProperties.Size = new System.Drawing.Size(200, 150);
+            this.pnlNoOfProperties.TabIndex = 11;
+            // 
+            // lblCountValue
+            // 
+            this.lblCountValue.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblCountValue.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.lblCountValue.Location = new System.Drawing.Point(0, 130);
+            this.lblCountValue.Name = "lblCountValue";
+            this.lblCountValue.Size = new System.Drawing.Size(200, 20);
+            this.lblCountValue.TabIndex = 8;
+            this.lblCountValue.Text = "Antal";
+            this.lblCountValue.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pnlPrice
+            // 
+            this.pnlPrice.Controls.Add(this.lblPriceValue);
+            this.pnlPrice.Controls.Add(this.mtrPrice);
+            this.pnlPrice.Controls.Add(this.lblPriceTitle);
+            this.pnlPrice.Location = new System.Drawing.Point(270, 35);
+            this.pnlPrice.Name = "pnlPrice";
+            this.pnlPrice.Size = new System.Drawing.Size(200, 150);
+            this.pnlPrice.TabIndex = 12;
+            // 
+            // lblPriceValue
+            // 
+            this.lblPriceValue.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblPriceValue.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPriceValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.lblPriceValue.Location = new System.Drawing.Point(0, 130);
+            this.lblPriceValue.Name = "lblPriceValue";
+            this.lblPriceValue.Size = new System.Drawing.Size(200, 20);
+            this.lblPriceValue.TabIndex = 8;
+            this.lblPriceValue.Text = "Pris";
+            this.lblPriceValue.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // mtrPrice
+            // 
+            this.mtrPrice.CircleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.mtrPrice.CirclePoints = 3;
+            this.mtrPrice.CircleSize = 175;
+            this.mtrPrice.Location = new System.Drawing.Point(0, 30);
+            this.mtrPrice.Margin = new System.Windows.Forms.Padding(20);
+            this.mtrPrice.MaxValue = 60;
+            this.mtrPrice.MinValue = 0;
+            this.mtrPrice.Name = "mtrPrice";
+            this.mtrPrice.PointerColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.mtrPrice.Size = new System.Drawing.Size(200, 90);
+            this.mtrPrice.TabIndex = 0;
+            this.mtrPrice.Text = "meter1";
+            this.mtrPrice.Value = 55;
+            // 
+            // lblPriceTitle
+            // 
+            this.lblPriceTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblPriceTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPriceTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.lblPriceTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblPriceTitle.Name = "lblPriceTitle";
+            this.lblPriceTitle.Size = new System.Drawing.Size(200, 20);
+            this.lblPriceTitle.TabIndex = 7;
+            this.lblPriceTitle.Text = "Samlet salgspris";
+            this.lblPriceTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Overview
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.pnlPrice);
+            this.Controls.Add(this.pnlNoOfProperties);
+            this.Controls.Add(this.hdrDials);
+            this.Controls.Add(this.hdrOngoingCases);
+            this.Controls.Add(this.clsActiveCases);
+            this.Name = "Overview";
+            this.Size = new System.Drawing.Size(1135, 560);
+            this.pnlNoOfProperties.ResumeLayout(false);
+            this.pnlPrice.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private GraphicalMeterLibrary.Meter meter1;
-		private GraphicalMeterLibrary.Meter meter2;
-		private SmartControls.Header header1;
+        private GraphicalMeterLibrary.Meter mtrCount;
 		private SmartControls.ControlList clsActiveCases;
+        private System.Windows.Forms.Label lblCountTitle;
+        private Header hdrOngoingCases;
+        private Header hdrDials;
+        private System.Windows.Forms.Panel pnlNoOfProperties;
+        private System.Windows.Forms.Label lblCountValue;
+        private System.Windows.Forms.Panel pnlPrice;
+        private System.Windows.Forms.Label lblPriceValue;
+        private GraphicalMeterLibrary.Meter mtrPrice;
+        private System.Windows.Forms.Label lblPriceTitle;
 	}
 }
